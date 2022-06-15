6eb70183998f6875948b00e58bbbb8fd862f2694
Steve Beaudry <live.remix@gmail.com>
2022-06-15 17:19:11 -0400

    testing a pusher fix

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,7 +10,7 @@ window.Echo = new Echo({                                                    //
     broadcaster: 'pusher',                                                  //
     key: process.env.MIX_PUSHER_APP_KEY,                                    //
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,                            //
-    encrypted: true                                                         //
+    forceTLS: true                                                          //
 });                                                                     //////
 
 require('./dashboard.js');
