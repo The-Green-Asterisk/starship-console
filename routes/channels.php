89b76baaaaedc937feb57d872878839d3cb19696
Steve Beaudry <live.remix@gmail.com>
2022-06-15 20:06:23 -0400

    pusher is fixed now

--- routes/channels.php
+++ routes/channels.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Broadcast;
 */
 
 Broadcast::channel('presenceStarshipConsole.{starshipId}', function ($user, $starshipId) {
-    if ($user->is_dm || $user->characters->where('is_active', true)->where('starship_id', $starshipId)->exists())
+    if ($user->is_dm || $user->characters->where('is_active', true)->where('starship_id', $starshipId)->count() > 0)
         return $user->toArray();
 });
