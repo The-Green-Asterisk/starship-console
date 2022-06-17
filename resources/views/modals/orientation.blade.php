cff787a411d1cbe257b9b56d3ef45d9f5e08e7c4
Steve Beaudry <live.remix@gmail.com>
2022-06-16 22:07:38 -0400

    video size fix

--- resources/views/modals/orientation.blade.php
+++ resources/views/modals/orientation.blade.php
@@ -3,9 +3,8 @@
 @section('content')
     <iframe
         src="https://www.youtube-nocookie.com/embed/k0czLNbuE58?controls=0"
-        title="YouTube video player"
         frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         autoplay
         allowfullscreen></iframe>
 @endsection
