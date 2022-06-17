9102c4c33aa9e59582e225d9bac424f00f6c6cde
Steve Beaudry <live.remix@gmail.com>
2022-06-16 22:44:57 -0400

    upgraded video quality

--- resources/views/modals/orientation.blade.php
+++ resources/views/modals/orientation.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <iframe
-        src="https://www.youtube-nocookie.com/embed/k0czLNbuE58?controls=0"
+        src="https://www.youtube-nocookie.com/embed/hi4gQLbPMFA?controls=0"
         frameborder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         autoplay
