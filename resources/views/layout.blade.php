21a1e0d6a6bb9f11b569c1e3ed8d5753d2613be3
Steve Beaudry <live.remix@gmail.com>
2022-06-24 22:29:52 -0400

    nevermind

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,7 +8,6 @@
         <title>{{ $title ?? env("APP_NAME") }}</title>
         <link rel="icon" href="{{ url('/img/valkur-favicon.svg') }}">
         <link href="{{ url('/css/app.css') }}" rel="stylesheet">
-        <script src="https://cdn.tiny.cloud/1/o51weqkkyqtt9zegndzt9c8khol7waxhxpcmbfmjg8o5nr82/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 
     </head>
     <body id="body" class="fadein">
@@ -20,15 +19,5 @@
                 success('{{ session()->get('success') }}');
             </script>
         @endif
-        <script>
-            tinymce.init({
-              selector: 'textarea',
-              plugins: 'a11ychecker advcode casechange export formatpainter image editimage linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tableofcontents tinycomments tinymcespellchecker',
-              toolbar: 'a11ycheck addcomment showcomments casechange checklist code export formatpainter image editimage pageembed permanentpen table tableofcontents',
-              toolbar_mode: 'floating',
-              tinycomments_mode: 'embedded',
-              tinycomments_author: 'Author name',
-            });
-          </script>
     </body>
 </html>
