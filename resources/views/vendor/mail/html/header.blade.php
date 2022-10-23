cdb3ee03a52637425f761686972a81b1d96c49d8
Steve Beaudry <live.remix@gmail.com>
2022-10-23 15:31:10 -0400

    ...

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
     <a href="https://starship-console.catharicosa.com" style="color:#4caf50">
-        <img src="https://starship-console.catharicosa.com/storage/img/valkur-favicon.png" alt="Valkur" style="width:100px;" />
+        <img src="https://starship-console.catharicosa.com/img/valkur-favicon.png" alt="Valkur" style="width:100px;" />
     </a>
     <br/>
 <a href="{{ $url }}" style="display: inline-block;">
