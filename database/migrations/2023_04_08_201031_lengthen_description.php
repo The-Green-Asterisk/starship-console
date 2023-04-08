2556a831dc2e2485f28c8e4fb22328a5bb0dc7f8
Steve Beaudry <live.remix@gmail.com>
2023-04-08 16:11:35 -0400

    lengthen description column

---
+++ database/migrations/2023_04_08_201031_lengthen_description.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //change description column to text
+        Schema::table('cargo_items', function (Blueprint $table) {
+            $table->text('description')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //change description column to string
+        Schema::table('cargo_items', function (Blueprint $table) {
+            $table->string('description')->nullable()->change();
+        });
+    }
+};
