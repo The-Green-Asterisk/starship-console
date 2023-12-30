95b92dc4d8311087da1f956ea3f2db97a889a854
Steve Beaudry <live.remix@gmail.com>
2023-12-30 01:20:50 -0500

    boring update

--- routes/web.php
+++ routes/web.php
@@ -29,12 +29,7 @@ Route::post('update', function (Request $request) {
     if ($request->header('X-GitHub-Event') != 'push') {
         return response('OK', 200);
     }
-
-    file_put_contents(
-        'update.txt',
-        shell_exec('sudo .././update.sh')
-    );
-
+    shell_exec('.././update.sh');
     return response('OK', 200);
 });
 
